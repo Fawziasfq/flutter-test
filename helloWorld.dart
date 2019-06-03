6de942e30df7b6119f7b4f61fc42ecaf3a3c8e49
Fawziasfq <732598573@qq.com>
2019-06-03 08:58:40 +0800

    计数器实例

--- helloWorld.dart
+++ helloWorld.dart
@@ -1,5 +1,20 @@
-import 'package:flutter/material.dart';
+Stream.fromFutures([
+  // 1秒后返回结果
+  Future.delayed(new Duration(seconds: 1), () {
+    return "hello 1";
+  }),
+  // 抛出一个异常
+  Future.delayed(new Duration(seconds: 2),(){
+    throw AssertionError("Error");
+  }),
+  // 3秒后返回结果
+  Future.delayed(new Duration(seconds: 3), () {
+    return "hello 3";
+  })
+]).listen((data){
+   print(data);
+}, onError: (e){
+   print(e.message);
+},onDone: (){
 
-void main() {
-  runApp(new Center(child: new Text('Hello, world!', textDirection: TextDirection.ltr,),))
-}
+});
